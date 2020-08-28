776d59f7ee4c8b385c7ba2619ab0a20d206ca95e
Francesco-Tromellini <63305263+Francesco-Tromellini@users.noreply.github.com>
2020-08-28 14:22:48 +0200

    Troisième graphique ajouté: productivité
    Dans ce graphique, je montre la productivité de chaque culture par hectare année par année.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // Paramètres de la visualisation
 const width = 400;
 const height = 300;
-const margin = { top: 20, right: 20, bottom: 20, left: 40 };
+const margin = { top: 30, right: 20, bottom: 30, left: 40 };
 
 let productionData ;
 let matScaleX, matScaleY ;
@@ -12,6 +12,10 @@ let matBars ;
 let hecBars ;
 let matTitles ;
 let hecTitles ;
+let phScaleX, phScaleY ;
+let phColorScale ;
+let phBars ;
+let phTitles ;
 let currentVariety = 'Carnaroli';
 
 const varieties = [
@@ -29,6 +33,8 @@ function setup () {
    setupProductionOf();
 
    setupHectaresOf();
+
+ setupProdOnHec();
 
 }
 
@@ -63,6 +69,7 @@ function onDataLoaded(data){
          })
    graphHectaresOf();
    graphProductionOf();
+   graphProdOnHec();
 }
 
 function setupHectaresOf(){
@@ -149,6 +156,91 @@ function graphHectaresOf(){
       .attr('y', d => hecScaleY(d.hectares))
       .text(d => d.hectares)
       
+}
+
+function setupProdOnHec(){
+ // élément SVG et le configurer
+ const svg = d3.select('body')
+ .append('svg')
+ .attr('width', width)
+ .attr('height', height)
+ .attr('style', 'font: 10px sans-serif');
+
+ // échelle horizontale
+ phScaleX = d3.scaleBand()
+ .domain([2016, 2017, 2018, 2019])
+ .range([margin.left, width - margin.right])
+ .padding(0.1)
+ .round(true);
+
+ // échelle verticale
+ phScaleY = d3.scaleLinear()
+ .domain([0, 90])
+ .range([height - margin.bottom - 5, margin.top])
+ .interpolate(d3.interpolateRound);
+
+ // échelle de couleur
+ phColorScale = d3.scaleSequential()
+ .domain ([0, 90])
+ .interpolator(d3.interpolateBlues);
+
+ // barres et titres
+ phBars = svg.append('g');
+ phTitles = svg.append('g')
+ .style('fill', 'white')
+ .attr('text-anchor', 'middle')
+ .attr('transform', `translate(${phScaleX.bandwidth() / 2}, 6)`);
+
+ // axe horizontal
+ svg.append('g')
+ .attr('transform', `translate(0, ${height - margin.bottom})`)
+ .call(d3.axisBottom(phScaleX))
+ .call(g => g.select('.domain').remove())
+
+ // axe vertical
+ svg.append('g')
+ .attr('transform', `translate(${margin.left}, 0)`)
+ .call(d3.axisLeft(phScaleY))
+ .call(g => g.select('.domain').remove())
+
+ // titre du graphique
+ svg.append('text')
+ .attr('font-size', '18px')
+ .attr('class', 'title')
+ .attr('x', width / 2 + margin.left / 2)
+ .attr('y', 20)
+ .attr('text-anchor', 'middle')
+ .text('Productivité (Prod./Hectar)')
+ .style('fill', 'steelblue')
+ .style('underline')
+
+}
+
+function graphProdOnHec(){
+   const data = productionData.filter(d => d.variety === currentVariety)
+
+   // Barres
+   phBars.selectAll('rect')
+   .data(data)
+   .join('rect')
+      .transition()
+      .duration(1000)
+      .attr('width', phScaleX.bandwidth())
+      .attr('height', d => phScaleY(0) - phScaleY(d.production/d.hectares))
+      .attr('x', d => phScaleX(d.year))
+      .attr('y', d => phScaleY(d.production/d.hectares))
+      .style('fill', d => phColorScale(d.production/d.hectares))
+
+   // Titres
+   phTitles.selectAll('text')
+   .data(data)
+   .join('text')
+      .transition()
+      .duration(1000)
+      .attr('dy', '0.35em')
+      .attr('x', d => phScaleX(d.year))
+      .attr('y', d => phScaleY(d.production/d.hectares))
+      .text(d => Math.ceil(d.production/d.hectares))
 }
 
 function setupProductionOf(){
